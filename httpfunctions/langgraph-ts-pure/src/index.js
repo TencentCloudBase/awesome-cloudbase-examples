3742bca0d9ad9968f3145878c67051843712357b
Areo-Joe <1047726015@qq.com>
2026-01-04 16:07:25 +0800

    feat: pure langgraph

---
+++ httpfunctions/langgraph-ts-pure/src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { createExpressRoutes } from "@cloudbase/agent-server";
+import { createAgent } from "./agent.js";
+import cors from "cors";
+import dotenvx from "@dotenvx/dotenvx";
+import { checkOpenAIEnvMiddleware } from "./utils.js";
+dotenvx.config();
+const app = express();
+app.use(cors());
+app.use(checkOpenAIEnvMiddleware);
+createExpressRoutes({
+    createAgent,
+    express: app,
+});
+app.listen(9000, () => console.log("Listening on 9000!"));
