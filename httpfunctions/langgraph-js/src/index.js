3945116360ba9e9a297ad7efb46b11ab650ca034
Areo-Joe <1047726015@qq.com>
2026-01-04 20:23:54 +0800

    chore: format

--- httpfunctions/langgraph-js/src/index.js
+++ httpfunctions/langgraph-js/src/index.js
@@ -5,11 +5,14 @@ import cors from "cors";
 import dotenvx from "@dotenvx/dotenvx";
 import { checkOpenAIEnvMiddleware } from "./utils.js";
 dotenvx.config();
+
 const app = express();
 app.use(cors());
 app.use(checkOpenAIEnvMiddleware);
+
 createExpressRoutes({
-    createAgent,
-    express: app,
+  createAgent,
+  express: app,
 });
+
 app.listen(9000, () => console.log("Listening on 9000!"));
