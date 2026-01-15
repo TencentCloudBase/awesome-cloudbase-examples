5c8475e6945a1e1ab6ab5ae55b533987b7262cb5
adamsyu <adamsyu@tencent.com>
2026-01-15 19:30:03 +0800

    feat: add cloudbase-flutter-example

---
+++ example/cloudbase-flutter-example/lib/app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      debugShowCheckedModeBanner: false,
+      navigatorKey: navigatorKey, // 用于验证码弹窗
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.system,
+      home: const HomePage(),
+    );
+  }
+}
